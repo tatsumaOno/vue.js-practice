cbe227dec8e3d6022635f71224e8912a3d763342
tatsumaOno <onotatsuma@onodarumanoMacBook-puro.local>
2019-05-08 23:33:58 +0900

    add comment quite api

--- realtime_search_start/js/main.js
+++ realtime_search_start/js/main.js
@@ -6,6 +6,7 @@ var app = new Vue({
         message: ''
     },
     watch: {
+        //keywordが入力されたら発火。
         keyword: function(newKeyword, oldKeyword){
             this.message = 'Waiting for you to stop typing...'
             this.debouncedGetAnswer()
@@ -13,29 +14,32 @@ var app = new Vue({
 
     },
     created: function() {
+        //keywordが入力されるたびにgetAnswerが発火すると、困るので
+        //アクセスに制限をかけるために、ユーザーの入力が終わるのを待ってからajax通信でリクエストを実行している。
+        //https://lodash.com/docs#debounce
         this.debouncedGetAnswer = _.debounce(this.getAnswer,1000)
     },
     methods: {
         getAnswer: function(){
-            //keywordが空ならitemsを空にして返却。
+            //keywordが空ならitemsとmessageを空にして返却。
             if(this.keyword === ''){
                 this.items = null
                 this.message = ''
                 return
             }
             this.message = 'Loading...'
-            //pageがqiitaの1ページ目,20は記事数
+            //pageがqiitaの1ページ目,20は記事数,入力されたkeywordをparamsに代入。
             var params = {page: 1,per_page: 20, query: this.keyword }
             //axiosでqiitaAPIを叩いている。
-            var vm = this
             axios.get('https://qiita.com/api/v2/items',{params}).then(function(response){
                 this.items = response.data
-                
+                //レスポンスをitemsに代入.
             }.bind(this))
             .catch(function(error){
                 this.message = 'Error!'+ error
             })
             .finally(function(){
+                //処理が終わるとmessageをからにしている。
                 this.message = ''
             }.bind(this))
         }
