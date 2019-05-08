6a55d77385d8454a2eb314121e1499bcc6ea149f
tatsumaOno <onotatsuma@onodarumanoMacBook-puro.local>
2019-05-08 23:14:51 +0900

    edit main.js quit

--- realtime_search_start/js/main.js
+++ realtime_search_start/js/main.js
@@ -6,36 +6,38 @@ var app = new Vue({
         message: ''
     },
     watch: {
+        keyword: function(newKeyword, oldKeyword){
+            this.message = 'Waiting for you to stop typing...'
+            this.debouncedGetAnswer()
+        }
 
     },
     created: function() {
-        //インスタンスが作成されたら実行される
-        this.keyword = "javaScript"
-        this.getAnswer()
+        this.debouncedGetAnswer = _.debounce(this.getAnswer,1000)
     },
     methods: {
         getAnswer: function(){
             //keywordが空ならitemsを空にして返却。
             if(this.keyword === ''){
                 this.items = null
+                this.message = ''
                 return
             }
             this.message = 'Loading...'
             //pageがqiitaの1ページ目,20は記事数
             var params = {page: 1,per_page: 20, query: this.keyword }
             //axiosでqiitaAPIを叩いている。
+            var vm = this
             axios.get('https://qiita.com/api/v2/items',{params}).then(function(response){
                 this.items = response.data
-                console.log(items)
-            })
-            //catchでエラーが出たらmessageに代入する
+                
+            }.bind(this))
             .catch(function(error){
                 this.message = 'Error!'+ error
             })
-            //最後にmessageを空にする。
             .finally(function(){
                 this.message = ''
-            })
+            }.bind(this))
         }
 
     }
