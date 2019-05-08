36960a2cac5fb009db2bf2c4378f01c50e9f9f91
tatsumaOno <onotatsuma@onodarumanoMacBook-puro.local>
2019-05-08 20:43:03 +0900

    qiita記事を取得するための部分を作成

--- realtime_search_start/js/main.js
+++ realtime_search_start/js/main.js
@@ -1,15 +1,42 @@
 var app = new Vue({
     el: '#app',
     data: {
-
+        items: null,
+        keyword: '',
+        message: ''
     },
     watch: {
 
     },
     created: function() {
-
+        //インスタンスが作成されたら実行される
+        this.keyword = "javaScript"
+        this.getAnswer()
     },
     methods: {
+        getAnswer: function(){
+            //keywordが空ならitemsを空にして返却。
+            if(this.keyword === ''){
+                this.items = null
+                return
+            }
+            this.message = 'Loading...'
+            //pageがqiitaの1ページ目,20は記事数
+            var params = {page: 1,per_page: 20, query: this.keyword }
+            //axiosでqiitaAPIを叩いている。
+            axios.get('https://qiita.com/api/v2/items',{params}).then(function(response){
+                this.items = response.data
+                console.log(items)
+            })
+            //catchでエラーが出たらmessageに代入する
+            .catch(function(error){
+                this.message = 'Error!'+ error
+            })
+            //最後にmessageを空にする。
+            .finally(function(){
+                this.message = ''
+            })
+        }
 
     }
 })
